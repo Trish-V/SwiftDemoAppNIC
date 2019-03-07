8945a1c2eb93321b65a52b6dbf674fb55d88af89
Viroj Fernando <virojfdo@live.com>
2019-03-07 19:06:32 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoSwiftApp
+//
+//  Created by Viroj Fernando on 3/6/19.
+//  Copyright © 2019 Viroj Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var students : [String] = ["saman","namal","viroj"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
